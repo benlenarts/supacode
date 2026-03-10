0c743226649265b3658f5b8c9598b433012ca4e2
khoi <6994441+khoi@users.noreply.github.com>
2026-03-10 16:58:13 +0000

    Fix release analytics client build

--- supacode/Clients/Analytics/AnalyticsClient.swift
+++ supacode/Clients/Analytics/AnalyticsClient.swift
@@ -1,6 +1,5 @@
 import ComposableArchitecture
 import PostHog
-import SwiftUI
 
 struct AnalyticsClient: Sendable {
   var capture: @Sendable (_ event: String, _ properties: [String: Any]?) -> Void
@@ -11,15 +10,11 @@ extension AnalyticsClient: DependencyKey {
   static let liveValue = AnalyticsClient(
     capture: { event, properties in
       #if !DEBUG
-        @Shared(.settingsFile) var settingsFile
-        guard settingsFile.global.analyticsEnabled else { return }
         PostHogSDK.shared.capture(event, properties: properties)
       #endif
     },
     identify: { distinctId in
       #if !DEBUG
-        @Shared(.settingsFile) var settingsFile
-        guard settingsFile.global.analyticsEnabled else { return }
         PostHogSDK.shared.identify(distinctId)
       #endif
     }
@@ -35,16 +30,5 @@ extension DependencyValues {
   var analyticsClient: AnalyticsClient {
     get { self[AnalyticsClient.self] }
     set { self[AnalyticsClient.self] = newValue }
-  }
-}
-
-private struct AnalyticsClientKey: EnvironmentKey {
-  static let defaultValue = AnalyticsClient.liveValue
-}
-
-extension EnvironmentValues {
-  var analyticsClient: AnalyticsClient {
-    get { self[AnalyticsClientKey.self] }
-    set { self[AnalyticsClientKey.self] = newValue }
   }
 }
